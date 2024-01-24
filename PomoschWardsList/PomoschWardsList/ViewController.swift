b0c02c2e9221ffdac8e653d4d4cf86315a185966
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2024-01-24 23:03:11 +0300

    initial commit

---
+++ PomoschWardsList/PomoschWardsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PomoschWardsList
+//
+//  Created by Роман Козловский on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
